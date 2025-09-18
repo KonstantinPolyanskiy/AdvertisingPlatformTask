8d539e12cd4f4948e8597aff2e7585651d5bf261
polyanskiy <polyanskiy.main@list.ru>
2025-09-18 20:57:47 +0300

    При запуске приложения открывается swagger

--- Core.WebApi/Program.cs
+++ Core.WebApi/Program.cs
@@ -24,7 +24,11 @@ var app = builder.Build();
 app.UseRouting();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.MapControllers();
 
